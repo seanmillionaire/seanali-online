cdfae3c599a3c8cfd65b7ee8f5b3fd84bca6c04e
seanmillionaire <158592572+seanmillionaire@users.noreply.github.com>
2026-07-05 19:06:46 -0500

    Load Country Gems rename helper

--- games/sounds.js
+++ games/sounds.js
@@ -12,7 +12,10 @@
       scripts.push({ global: 'GameMistakeReaderLoaded', src: '/games/mistake-reader.js?v=1' });
     }
     if (gamePath === '/games/elements/') scripts.push({ global: 'ElementsClueBarLoaded', src: '/games/elements/clue-bar.js?v=1' });
-    if (gamePath === '/games/family-gems/') scripts.push({ global: 'FamilyGemsClueRiddlesLoaded', src: '/games/family-gems/clue-riddles.js?v=1' });
+    if (gamePath === '/games/family-gems/') {
+      scripts.push({ global: 'FamilyGemsClueRiddlesLoaded', src: '/games/family-gems/clue-riddles.js?v=1' });
+      scripts.push({ global: 'CountryGemsNameLoaded', src: '/games/family-gems/country-name.js?v=1' });
+    }
     if (gamePath === '/games/piano/') scripts.push({ global: 'PianoFreestyleLoaded', src: '/games/piano/freestyle.js?v=1' });
 
     scripts.forEach(item => {
